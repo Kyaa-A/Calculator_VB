da4cb303ca415c1c2c0c102e8a297d0baacab82c
Kyaa-A <asnaripacalna@gmail.com>
2025-01-30 21:38:00 +0800

    Add project files.

---
+++ practice/Calculator.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Calculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        TextBox1 = New TextBox()
+        Button16 = New Button()
+        Button17 = New Button()
+        TextBox2 = New TextBox()
+        Button18 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button1.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button1.Location = New Point(29, 180)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(79, 67)
+        Button1.TabIndex = 0
+        Button1.Text = "7"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button2.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button2.Location = New Point(114, 180)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(79, 67)
+        Button2.TabIndex = 1
+        Button2.Text = "8"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button3.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button3.Location = New Point(199, 180)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(79, 67)
+        Button3.TabIndex = 2
+        Button3.Text = "9"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button4.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button4.Location = New Point(369, 264)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(79, 67)
+        Button4.TabIndex = 3
+        Button4.Text = "÷"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button5.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button5.Location = New Point(29, 264)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(79, 67)
+        Button5.TabIndex = 4
+        Button5.Text = "4"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button6.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button6.Location = New Point(114, 264)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(79, 67)
+        Button6.TabIndex = 5
+        Button6.Text = "5"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button7.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button7.Location = New Point(199, 264)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(79, 67)
+        Button7.TabIndex = 6
+        Button7.Text = "6"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button8.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button8.Location = New Point(283, 264)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(79, 67)
+        Button8.TabIndex = 7
+        Button8.Text = "x"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button9.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button9.Location = New Point(29, 347)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(79, 67)
+        Button9.TabIndex = 8
+        Button9.Text = "1"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button10.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button10.Location = New Point(114, 347)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(79, 67)
+        Button10.TabIndex = 9
+        Button10.Text = "2"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button11.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button11.Location = New Point(199, 347)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(79, 67)
+        Button11.TabIndex = 10
+        Button11.Text = "3"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button12.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button12.Location = New Point(283, 347)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(79, 67)
+        Button12.TabIndex = 11
+        Button12.Text = "-"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button13.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button13.Location = New Point(29, 431)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(79, 67)
+        Button13.TabIndex = 12
+        Button13.Text = "."
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button14.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button14.Location = New Point(114, 431)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(79, 67)
+        Button14.TabIndex = 13
+        Button14.Text = "0"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = Color.Red
+        Button15.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button15.ForeColor = Color.White
+        Button15.Location = New Point(283, 431)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(165, 67)
+        Button15.TabIndex = 14
+        Button15.Text = "="
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(29, 76)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(419, 81)
+        TextBox1.TabIndex = 15
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = Color.FromArgb(CByte(228), CByte(203), CByte(195))
+        Button16.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button16.Location = New Point(369, 347)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(79, 67)
+        Button16.TabIndex = 16
+        Button16.Text = "+"
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = Color.FromArgb(CByte(100), CByte(116), CByte(154))
+        Button17.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button17.ForeColor = Color.White
+        Button17.Location = New Point(283, 180)
+        Button17.Name = "Button17"
+        Button17.RightToLeft = RightToLeft.No
+        Button17.Size = New Size(165, 67)
+        Button17.TabIndex = 17
+        Button17.Text = "Clear"
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.Location = New Point(29, 23)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(419, 35)
+        TextBox2.TabIndex = 18
+        TextBox2.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Button18
+        ' 
+        Button18.Location = New Point(202, 431)
+        Button18.Margin = New Padding(3, 4, 3, 4)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(74, 67)
+        Button18.TabIndex = 19
+        Button18.Text = "🍵 Buy  Coffee"
+        Button18.UseVisualStyleBackColor = True
+        ' 
+        ' Calculator
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(55), CByte(63), CByte(93))
+        ClientSize = New Size(477, 528)
+        Controls.Add(Button18)
+        Controls.Add(TextBox2)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(TextBox1)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Calculator"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button18 As Button
+
+End Class
---
+++ practice/Calculator.vb
@@ -0,0 +1,154 @@
+﻿Public Class Calculator
+    Dim firstOperand As Double
+    Dim secondOperand As Double
+    Dim [operator] As String
+    Dim isOperatorClicked As Boolean = False
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text + "7"
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        firstOperand = 0
+        secondOperand = 0
+        [operator] = ""
+        isOperatorClicked = False
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text + "1"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        TextBox1.Text = TextBox1.Text + "2"
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        TextBox1.Text = TextBox1.Text + "3"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text + "4"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text + "5"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text + "6"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text + "8"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text + "9"
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        'For "="
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            secondOperand = Double.Parse(TextBox1.Text)
+            Dim result As Double = 0
+
+            Select Case [operator]
+                Case "+"
+                    result = firstOperand + secondOperand
+                Case "-"
+                    result = firstOperand - secondOperand
+                Case "*"
+                    result = firstOperand * secondOperand
+                Case "/"
+                    If secondOperand <> 0 Then
+                        result = firstOperand / secondOperand
+                    Else
+                        MessageBox.Show("Cannot divide by zero!")
+                        Return
+                    End If
+            End Select
+
+            TextBox2.Text = TextBox2.Text + TextBox1.Text + " = " + result.ToString()
+            TextBox1.Text = result.ToString()
+            isOperatorClicked = False
+        End If
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        'For "+"
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            firstOperand = Double.Parse(TextBox1.Text)
+            [operator] = "+"
+            TextBox2.Text = TextBox1.Text + " + "
+            TextBox1.Clear()
+            isOperatorClicked = True
+        End If
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        'For "-"
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            firstOperand = Double.Parse(TextBox1.Text)
+            [operator] = "-"
+            TextBox2.Text = TextBox1.Text + " - "
+            TextBox1.Clear()
+            isOperatorClicked = True
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        'For "*"
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            firstOperand = Double.Parse(TextBox1.Text)
+            [operator] = "*"
+            TextBox2.Text = TextBox1.Text + " * "
+            TextBox1.Clear()
+            isOperatorClicked = True
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'For "/"
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            firstOperand = Double.Parse(TextBox1.Text)
+            [operator] = "/"
+            TextBox2.Text = TextBox1.Text + " / "
+            TextBox1.Clear()
+            isOperatorClicked = True
+        End If
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        'For "."
+        If Not TextBox1.Text.Contains(".") Then
+            TextBox1.Text = TextBox1.Text + "."
+        End If
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        TextBox1.Text = TextBox1.Text + "0"
+    End Sub
+
+    Private Sub Calculator_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        Try
+            ' Use ProcessStartInfo to open the URL in the default browser
+            Dim psi As New ProcessStartInfo("https://github.com/Kyaa-A")
+            psi.UseShellExecute = True
+            Process.Start(psi)
+        Catch ex As Exception
+            ' Handle any errors
+            MessageBox.Show("Error opening URL: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Calculator_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
